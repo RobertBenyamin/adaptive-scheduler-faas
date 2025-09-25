cb2667bbc9c468683c14ed47e8c128e85207fa18
RobertBenyamin <robertbenyamino@gmail.com>
2025-09-25 16:32:08 +0700

    Update DNS configuration with new IP address for Knative Serving

--- deploy_only.sh
+++ deploy_only.sh
@@ -21,4 +21,4 @@ sudo kubectl apply -f https://github.com/knative/serving/releases/download/knati
 sudo kubectl patch configmap/config-domain \
   -n knative-serving \
   --type merge \
-  -p '{"data":{"52.72.211.102.sslip.io":""}}'
+  -p '{"data":{"172.31.30.118.sslip.io":""}}'
