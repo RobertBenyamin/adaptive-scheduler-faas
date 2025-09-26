17625d597cade53e687675787c39dd4cdc2ae1fc
RobertBenyamin <robertbenyamino@gmail.com>
2025-09-26 12:05:21 +0700

    Update copy.sh to restore and copy all runner scripts for each service

--- copy.sh
+++ copy.sh
@@ -1,24 +1,29 @@
-# cp cnn_serving/runners_v2.py img_res/runners_v2.py
-# cp cnn_serving/runners_v3.py img_res/runners_v3.py
-cp cnn_serving/runner_v5.py img_res/runner_v5.py
-# cp cnn_serving/runners_v5.py img_res/runners_v5.py
+cp cnn_serving/runner.py img_res/runner.py
+cp cnn_serving/runners_v2.py img_res/runners_v2.py
+cp cnn_serving/runners_v3.py img_res/runners_v3.py
+cp cnn_serving/runners_v4.py img_res/runners_v4.py
+cp cnn_serving/runners_v5.py img_res/runners_v5.py
 
-# cp cnn_serving/runners_v2.py img_rot/runners_v2.py
-# cp cnn_serving/runners_v3.py img_rot/runners_v3.py
-cp cnn_serving/runner_v5.py img_rot/runner_v5.py
-# cp cnn_serving/runners_v5.py img_rot/runners_v5.py
+cp cnn_serving/runner.py img_rot/runner.py
+cp cnn_serving/runners_v2.py img_rot/runners_v2.py
+cp cnn_serving/runners_v3.py img_rot/runners_v3.py
+cp cnn_serving/runners_v4.py img_rot/runners_v4.py
+cp cnn_serving/runners_v5.py img_rot/runners_v5.py
 
-# cp cnn_serving/runners_v2.py ml_train/runners_v2.py
-# cp cnn_serving/runners_v3.py ml_train/runners_v3.py
-cp cnn_serving/runner_v5.py ml_train/runner_v5.py
-# cp cnn_serving/runners_v5.py ml_train/runners_v5.py
+cp cnn_serving/runner.py ml_train/runner.py
+cp cnn_serving/runners_v2.py ml_train/runners_v2.py
+cp cnn_serving/runners_v3.py ml_train/runners_v3.py
+cp cnn_serving/runners_v4.py ml_train/runners_v4.py
+cp cnn_serving/runners_v5.py ml_train/runners_v5.py
 
-# cp cnn_serving/runners_v2.py vid_proc/runners_v2.py
-# cp cnn_serving/runners_v3.py vid_proc/runners_v3.py
-cp cnn_serving/runner_v5.py vid_proc/runner_v5.py
-# cp cnn_serving/runners_v5.py vid_proc/runners_v5.py
+cp cnn_serving/runner.py vid_proc/runner.py
+cp cnn_serving/runners_v2.py vid_proc/runners_v2.py
+cp cnn_serving/runners_v3.py vid_proc/runners_v3.py
+cp cnn_serving/runners_v4.py vid_proc/runners_v4.py
+cp cnn_serving/runners_v5.py vid_proc/runners_v5.py
 
-# cp cnn_serving/runners_v2.py web_serve/runners_v2.py
-# cp cnn_serving/runners_v3.py web_serve/runners_v3.py
-cp cnn_serving/runner_v5.py web_serve/runner_v5.py
-# cp cnn_serving/runners_v5.py web_serve/runners_v5.py
+cp cnn_serving/runner.py web_serve/runner.py
+cp cnn_serving/runners_v2.py web_serve/runners_v2.py
+cp cnn_serving/runners_v3.py web_serve/runners_v3.py
+cp cnn_serving/runners_v4.py web_serve/runners_v4.py
+cp cnn_serving/runners_v5.py web_serve/runners_v5.py
