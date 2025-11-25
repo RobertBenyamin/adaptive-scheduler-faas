bb76224ea66e210dd146e248a6299172b1899620
RobertBenyamin <robertbenyamino@gmail.com>
2025-11-26 04:17:07 +0700

    Refactor lambda_handler: Update file handling to use temporary directory for image processing and streamline image download logic.

--- cnn_serving/app.py
+++ cnn_serving/app.py
@@ -1,29 +1,30 @@
 from mxnet import gluon
 import os
 import mxnet as mx
-from PIL import Image
 from storage_helper import download_file
 
-current_path = "/app/pythonAction"
+TMP_DIR = "/tmp/"
 
-net = gluon.model_zoo.vision.resnet50_v1(pretrained=True, root = '/tmp/')
+net = gluon.model_zoo.vision.resnet50_v1(pretrained=True, root = TMP_DIR)
 net.hybridize(static_alloc=True, static_shape=True)
-lblPath = gluon.utils.download('https://github.com/shicai/MobileNet-Caffe/blob/master/synset.txt',path='/tmp/')
+lblPath = gluon.utils.download('https://raw.githubusercontent.com/shicai/MobileNet-Caffe/refs/heads/master/synset.txt',path=TMP_DIR)
 with open(lblPath, 'r') as f:
     labels = [l.rstrip() for l in f]
 
 def lambda_handler(event):
     blobName = event.get("input_file", "img10.jpg")
-    download_file(blobName, f"{current_path}/{blobName}")
-    image = Image.open(f"{current_path}/{blobName}")
-    image.save(f'{current_path}/tempImage_'+str(os.getpid())+'.jpeg')
+    
+    pid = str(os.getpid())
+    local_file_path = os.path.join(TMP_DIR, f"{pid}_{blobName}")
+
+    download_file(blobName, local_file_path)
 
     # format image as (batch, RGB, width, height)
-    img = mx.image.imread(f'{current_path}/tempImage_'+str(os.getpid())+'.jpeg')
+    img = mx.image.imread(local_file_path)
     img = mx.image.imresize(img, 224, 224) # resize
     img = mx.image.color_normalize(img.astype(dtype='float32')/255,
                                 mean=mx.nd.array([0.485, 0.456, 0.406]),
-                                std=mx.nd.array([0.229, 0.224, 0.225])) # normal     ize
+                                std=mx.nd.array([0.229, 0.224, 0.225])) # normalize
     img = img.transpose((2, 0, 1)) # channel first
     img = img.expand_dims(axis=0) # batchify
 
