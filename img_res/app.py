6f91b68a8823880d8461e23566c3feb22a9621ea
RobertBenyamin <robertbenyamino@gmail.com>
2025-11-26 04:22:22 +0700

    Refactor lambda_handler: Update file handling to use a temporary directory for image processing and improve clarity in file paths.

--- img_res/app.py
+++ img_res/app.py
@@ -2,13 +2,17 @@ import os
 from PIL import Image
 from storage_helper import download_file, upload_file
 
-current_path = "/app/pythonAction"
+TMP_DIR = "/tmp/"
 
 def lambda_handler(event):
-    blobName = event.get("input_file", "img10.jpg")
-    download_file(blobName, f"{current_path}/{blobName}")
+    input_filename = event.get("input_file", "img10.jpg")
+    pid = str(os.getpid())
+    local_input_path = os.path.join(TMP_DIR, f"{pid}_{input_filename}")
     
-    image = Image.open(f"{current_path}/{blobName}")
+    download_file(input_filename, local_input_path)
+    
+    image = Image.open(local_input_path)
+
     width, height = image.size
     # Setting the points for cropped image
     left = 4
@@ -16,9 +20,12 @@ def lambda_handler(event):
     right = 100
     bottom = 3 * height / 5
     im1 = image.crop((left, top, right, bottom))
-    im1.save('tempImage_'+str(os.getpid())+'.jpeg')
 
-    blobName = "resized-"+blobName
-    upload_file(f"{current_path}/{blobName}", blobName)
+    output_filename = f"resized-{input_filename}" 
+    local_output_path = os.path.join(TMP_DIR, output_filename)
+
+    im1.save(local_output_path)
+
+    upload_file(local_output_path, output_filename)
 
     return {"Image":"resized"}
