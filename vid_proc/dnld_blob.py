6cf9f0699cbbef073bf22f79e14cb28093a5f282
RobertBenyamin <robertbenyamino@gmail.com>
2025-11-28 02:20:31 +0700

    Refactor blob download and upload functions to handle socket timeouts and improve data reception

--- vid_proc/dnld_blob.py
+++ vid_proc/dnld_blob.py
@@ -4,29 +4,55 @@ import os
 import socket
 import os
 import json
-import base64
-import time
 
 def download_blob_new(blobName):
     myHost = '0.0.0.0'
     myPort = 3333
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSocket.connect((myHost, myPort))
-    message = {"blobName": blobName, "operation": "get", "pid": os.getpid()}
-    messageStr = json.dumps(message)
-    clientSocket.sendall(messageStr.encode(encoding="utf-8"))
+    clientSocket.settimeout(5.0)
+    try:
+        clientSocket.connect((myHost, myPort))
+        message = {"blobName": blobName, "operation": "get", "pid": os.getpid()}
+        messageStr = json.dumps(message)
+        clientSocket.sendall(messageStr.encode(encoding="utf-8"))
 
-    data_ = b''
-    data_ += clientSocket.recv(1024)
+        data_ = b''
+        while True:
+            try:
+                chunk = clientSocket.recv(4096)
+                if not chunk:
+                    break
+                data_ += chunk
+            except socket.timeout:
+                break
+    except socket.timeout:
+        pass
+    finally:
+        clientSocket.close()
+    return data_
 
 def upload_blob_new(blobName, value):
     myHost = '0.0.0.0'
     myPort = 3333
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSocket.connect((myHost, myPort))
-    message = {"blobName": blobName, "operation": "set", "value": value, "pid": os.getpid()}
-    messageStr = json.dumps(message)
-    clientSocket.sendall(messageStr.encode(encoding="utf-8"))
+    clientSocket.settimeout(5.0)
+    try:
+        clientSocket.connect((myHost, myPort))
+        message = {"blobName": blobName, "operation": "set", "value": value, "pid": os.getpid()}
+        messageStr = json.dumps(message)
+        clientSocket.sendall(messageStr.encode(encoding="utf-8"))
 
-    data_ = b''
-    data_ += clientSocket.recv(1024)
+        data_ = b''
+        while True:
+            try:
+                chunk = clientSocket.recv(4096)
+                if not chunk:
+                    break
+                data_ += chunk
+            except socket.timeout:
+                break
+    except socket.timeout:
+        pass
+    finally:
+        clientSocket.close()
+    return data_
